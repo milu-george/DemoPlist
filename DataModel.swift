9c8d9fa414666d82036c12ca7ca84490eeea02f2
Milu Ann George <miluann@gmail.com>
2024-01-24 16:43:03 +0530

    commit data view model

---
+++ DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  DemoPlist
+//
+//  Created by Milu Ann George on 15/01/24.
+//
+
+import Foundation
---
+++ DataViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataViewModel.swift
+//  DemoPlist
+//
+//  Created by Milu Ann George on 15/01/24.
+//
+
+import Foundation
